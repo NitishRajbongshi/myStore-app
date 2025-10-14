df6447b7569cc9ed7759a9c3a04ec4f1963c2841
Nitish <nitishrajbongshi@gmail.com>
2025-10-14 13:55:22 +0530

    Fixed some styling

--- src/theme/constants.ts
+++ src/theme/constants.ts
@@ -41,16 +41,3 @@ export const defaultFontSizes = {
   xl: 24,
   xxl: 30,
 };
-
-export const defaultFontWeights = {
-  thin: '100',
-  extraLight: '200',
-  light: '300',
-  regular: '400',
-  medium: '500',
-  semiBold: '600',
-  bold: '700',
-  extraBold: '800',
-  black: '900',
-};
-
